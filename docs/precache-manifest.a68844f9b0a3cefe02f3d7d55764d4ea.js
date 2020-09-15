8730b79731bc3dbf1030c9fd2afc94bb4293655e
Gerardo Villegas <villegasgerardo.j@gmail.com>
2020-09-15 12:18:28 -0500

    Additing build

---
+++ docs/precache-manifest.a68844f9b0a3cefe02f3d7d55764d4ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f560207ecbafcd3bd6a1ab10e325dbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89f82fd0985f39dbc659",
+    "url": "/static/css/2.3807f13a.chunk.css"
+  },
+  {
+    "revision": "53a8a20a71f27183fd8a",
+    "url": "/static/css/main.7f59355e.chunk.css"
+  },
+  {
+    "revision": "89f82fd0985f39dbc659",
+    "url": "/static/js/2.d7356cda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d7356cda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53a8a20a71f27183fd8a",
+    "url": "/static/js/main.2d10dc99.chunk.js"
+  },
+  {
+    "revision": "974a9addee1cf7ab3b3f",
+    "url": "/static/js/runtime-main.b0bd413e.js"
+  },
+  {
+    "revision": "002f5facdd6a1679fe236242c9c37c7f",
+    "url": "/static/media/not-image.002f5fac.png"
+  }
+]);
